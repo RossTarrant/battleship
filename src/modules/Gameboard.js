0a01ea9cb4d32d63416b6ae4e57bc99a67910d77
Ross Tarrant <ross@Rosss-MBP.fritz.box>
2023-02-03 16:53:25 +0000

    Add place ship functionality (and testing)

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -15,10 +15,37 @@ export const Gameboard = () => {
 
     let missedAttacks = [];
 
-    const placeShip = (coords, ship) => {
-        for(let coord of coords){
-            board.set(coord, ship)
+    const placeShip = (coord, ship, axis) => {
+        const coords = coord.split(',');
+        let xCoord = parseInt(coords[0]);
+        let yCoord = parseInt(coords[1]);
+        let toPlace = [];
+        if(axis==='X'){
+            for(let i = 0; i < ship.length; i++){
+                if(board.get(`${[xCoord,yCoord]}`)===null && xCoord < 10){
+                    toPlace.push(`${[xCoord,yCoord]}`);
+                    xCoord++;
+                }
+                else{
+                    return null // Space already in use
+                }
+            }
         }
+        else if(axis==='Y'){
+            for(let i = 0; i < ship.length+1; i++){
+                if(board.get(`${[xCoord,yCoord]}`)===null && yCoord < 10){
+                    toPlace.push(`${[xCoord,yCoord]}`);
+                    yCoord++;
+                }
+                else{
+                    return null // Space already in use
+                }
+            }
+        }
+        for(let placement of toPlace){
+            board.set(placement, ship);
+        }
+        return true;
     }
 
     const receiveAttack = (coords) => {
--- src/modules/tests/Gameboard.test.js
+++ src/modules/tests/Gameboard.test.js
@@ -3,6 +3,34 @@ import { Ship } from "../Ship";
 
 describe('Gameboard Factory Testing', () => {
 
+    test('Ship is placed correctly on coordinates (X Axis)', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        expect(gameboard.placeShip('0,0', ship, 'X')).toBe(true);
+        expect(gameboard.placeShip('0,0', ship, 'X')).toBe(null);
+    });
+    
+    test('Ship is placed correctly on coordinates (Y Axis)', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        expect(gameboard.placeShip('0,0', ship, 'Y')).toBe(true);
+        expect(gameboard.placeShip('0,0', ship, 'Y')).toBe(null);
+    });
+
+    test('Ship is placed outside of X axis boundaries', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        expect(gameboard.placeShip('8,8', ship, 'X')).toBe(null);
+    });
+
+    test('Ship is placed on occupied coordinate', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(3);
+        gameboard.placeShip('0,0', ship, 'X');
+        const secondShip = Ship(3);
+        expect(gameboard.placeShip('0,0', secondShip, 'X')).toBe(null);
+    });
+
     test('Missed hit is recorded', () => {
         const gameboard = Gameboard();
         gameboard.receiveAttack('0,0');
@@ -12,8 +40,7 @@ describe('Gameboard Factory Testing', () => {
     test('Hit is recorded', () => {
         const gameboard = Gameboard();
         const ship = Ship(3);
-        const coords = ['0,0','0,1','0,2']
-        gameboard.placeShip(coords, ship);
+        gameboard.placeShip('0,0', ship, 'X');
         gameboard.receiveAttack('0,0');
         expect(ship.hits).toBe(1);
     });
