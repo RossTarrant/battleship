d5ca32a8bb95e21ea06fe8cfd2c4825d2726b4b6
Ross Tarrant <ross@Rosss-MBP.fritz.box>
2023-02-03 06:15:20 +0000

    Add test to Gameboard and add function

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,11 +1,35 @@
 export const Gameboard = () => {
 
-    let board = []; // Add data structure to contain gameboard
+    const buildBoard = () => {
+        let board = new Map();
+        for(let i = 0; i<10; i++){
+            for(let j = 0; j < 10; j ++){
+                let coord = `${[i,j]}`
+                board.set(coord, null);
+            }
+        }
+        return board;
+    }
+
+    let board = buildBoard();
+
+    let missedAttacks = [];
+
+    
 
     const receiveAttack = function(coords){
+        if(board.get(coords)===null){
+            missedAttacks.push(coords);
+        }
+    
+    }
+
+    const allShipsSunk = function(){
+
+
 
     }
 
-    return {receiveAttack};
+    return {receiveAttack, allShipsSunk, missedAttacks};
 
 }
--- src/modules/tests/Gameboard.test.js
+++ src/modules/tests/Gameboard.test.js
@@ -2,6 +2,10 @@ import { Gameboard } from "../Gameboard";
 
 describe('Gameboard Factory Testing', () => {
 
-    
+    test('Missed hit is recorded', () => {
+        const gameboard = Gameboard();
+        gameboard.receiveAttack('0,0');
+        expect(gameboard.missedAttacks.includes('0,0')).toBe(true);
+    });
 
 })
